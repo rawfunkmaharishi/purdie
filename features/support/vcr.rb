14cc49d0f5d8f95280e78bfc4e3e53c771135ee2
pikesley <github@orgraphone.org>
2015-02-28 23:49:01 +0000

    Tidy up

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -3,7 +3,7 @@ require 'webmock/cucumber'
 
 VCR.configure do |c|
   c.default_cassette_options = { :record => :once }
-  c.cassette_library_dir = 'fixtures/vcr'
+  c.cassette_library_dir = 'features/support/fixtures/vcr'
   c.hook_into :webmock
 end
 
