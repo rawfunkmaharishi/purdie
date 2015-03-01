5cc4e05c6ce77cbdaa1803dc28347ad642521cd7
pikesley <github@orgraphone.org>
2015-03-01 18:20:51 +0000

    Default title

--- lib/purdie/services/flickr.rb
+++ lib/purdie/services/flickr.rb
@@ -29,9 +29,8 @@ module Purdie
         results = {}
 
         results['title'] = photo['title']
+        results['title'] = @config['default-title'] if photo['title'] == ''
         results['date'] = photo['dates']['taken'].split(' ')[0]
-      #  require 'pry'
-      #  binding.pry
         results['photo_page'] = photo['urls'][0]['_content']
         results['photo_url'] = FlickRaw.url_m(photo)
 
@@ -40,8 +39,12 @@ module Purdie
         results['license'] = license['name'].split(' License')[0]
         results['license_url'] = license['url']
 
-        snapper = photo['tags'].select { |t| t['raw'] =~ /photographer/ }[0]
-        results['photographer'] = snapper['raw'].split(':')[1]
+        begin
+          snapper = photo['tags'].select { |t| t['raw'] =~ /photographer/ }[0]
+          results['photographer'] = snapper['raw'].split(':')[1]
+        rescue NoMethodError
+          results['photographer'] = @config['photographer-lookups'][photo['owner']['username']]
+        end
 
         results
       end
--- spec/flickr_spec.rb
+++ spec/flickr_spec.rb
@@ -17,8 +17,7 @@ module Purdie
 #      end
 
       it 'refines data for a photo', :vcr do
-        expect(@f.refine 'https://www.flickr.com/photos/rawfunkmaharishi/15631479625/
-').to eq({
+        expect(@f.refine 'https://www.flickr.com/photos/rawfunkmaharishi/15631479625/').to eq({
           "title"=>"The Comedy, October 2014",
           "date"=>"2014-10-22",
           "photo_page"=>"https://www.flickr.com/photos/rawfunkmaharishi/15631479625/",
@@ -26,6 +25,18 @@ module Purdie
           "license"=>"Attribution-NonCommercial-ShareAlike",
           "license_url"=>"https://creativecommons.org/licenses/by-nc-sa/2.0/",
           "photographer"=>"kim"
+        })
+      end
+
+      it 'refines data for a photo without a specific photogapher tag' do
+        expect(@f.refine 'https://www.flickr.com/photos/cluttercup/15950875724/').to eq ({
+          "title"=>"Raw Funk Maharishi",
+          "date"=>"2015-02-18",
+          "photo_page"=>"https://www.flickr.com/photos/cluttercup/15950875724/",
+          "photo_url"=>"https://farm8.staticflickr.com/7398/15950875724_23d58be214_m.jpg",
+          "license"=>"Attribution-NonCommercial",
+          "license_url"=>"https://creativecommons.org/licenses/by-nc/2.0/",
+          "photographer"=>"jane"
         })
       end
     end
