345dc79a94d488c9cf9612227f279c56d373a614
pikesley <github@orgraphone.org>
2015-03-09 18:56:07 +0000

    URL for size

--- spec/services/flickr_spec.rb
+++ spec/services/flickr_spec.rb
@@ -40,6 +40,28 @@ photographer: jane
       it 'falls back to the default photographer name', :vcr do
         expect(@f.distill('https://www.flickr.com/photos/pikesley/16649739916/')['photographer']).to eq 'pikesley'
       end
+
+      context 'FlickRaw url_ methods' do
+        it 'gives the plain (500px) method by default' do
+          expect(Flickr.url_for_size).to eq :url
+        end
+
+        it 'gives the 75ps url when we ask for 20 pixels' do
+          expect(Flickr.url_for_size 20).to eq :url_s
+        end
+
+        it 'gives the 640ps url when we ask for 600 pixels' do
+          expect(Flickr.url_for_size 600).to eq :url_z
+        end
+
+        it 'gives the 500px url when we ask for it' do
+          expect(Flickr.url_for_size 500).to eq :url
+        end
+
+        it 'gives the biggest url when we ask for something larger' do
+          expect(Flickr.url_for_size 6000000).to eq :url_o
+        end
+      end
     end
   end
 end
