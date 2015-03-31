05694e3610e6e90d9cbaa79465d7c8820c90028a
pikesley <github@orgraphone.org>
2015-03-31 19:05:37 +0100

    Can create sensible output

--- spec/bernard_spec.rb
+++ spec/bernard_spec.rb
@@ -7,6 +7,16 @@ module Purdie
       FileUtils.cp 'spec/support/fixtures/vimeo.vids', '_sources/'
       b = Bernard.new
       b.fetch
+
+      expect(File).to exist '_data/soundcloud.yaml'
+
+      lines = File.readlines '_data/soundcloud.yaml'
+      expect(lines).to include "- title: Safety On Board\n",
+                               "  id: 174628735\n",
+                               "  location: Enterprise Studios\n",
+                               "  date: '2014-10-14'\n",
+                               "  license: Attribution-NonCommercial-ShareAlike\n",
+                               "  license_url: http://creativecommons.org/licenses/by-nc-sa/4.0/\n"
     end
   end
 end
