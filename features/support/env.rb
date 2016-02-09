9221f528bc919fedbf2373f7ca4f4bd7b12be5da
pikesley <github@orgraphone.org>
2016-02-09 17:51:45 +0000

    Bump

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,10 @@
+require 'coveralls'
+Coveralls.wear_merged!
+
 ENV['RACK_ENV'] = 'test'
 
 require 'aruba/cucumber'
 require 'aruba/in_process'
 require 'rspec'
-
-require 'coveralls'
-Coveralls.wear_merged!
 
 $fixtures = File.join(File.dirname(__FILE__), 'fixtures/')
